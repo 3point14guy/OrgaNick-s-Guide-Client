0cfb16c0c0a17687ac32d2d910a8653163f6a9bf
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-08-30 16:06:18 -0400

    updated production URL in config file

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
-    production: 'http://localhost:4741'
+    production: 'https://boiling-dawn-26598.herokuapp.com'
     // temp chnaged form https://ga-wdi-boston.herokuapp.com to trouble shoot refresh problem.  2:25PM
   }
 }
