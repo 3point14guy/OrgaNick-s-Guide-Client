92cdf26b64a5967bba5fd5b5ef41b4472f690ada
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-09-10 11:46:08 -0400

    commented ui file

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,20 +1,27 @@
 'use strict'
 const store = require('./store')
-
+// getting access to handlebars files
 const displayVegetablesTemplate = require('./templates/vegetable-listing.handlebars')
 const displayGardenTemplate = require('./templates/your-garden-listing.handlebars')
 const displayPestsTemplate = require('./templates/pest-listing.handlebars')
 
 const signUpSuccess = (data) => {
+  // "." specifies class name, "#" specifies id name
+  // use jQuery to interact with elements
+  // .trigger manually calls action without user initiation
   $('.form-clear').trigger('reset')
+  // hides modal after submit button is selected
   $('#submit-register').modal('hide')
 }
 const signUpFailure = (error) => {
+  // displays message to user in div with class .sign-up-message.  .fadeIn has message appear, delay holds the message in place for 2000 microseconds, and fade out causes the message to slowly disappear
   $('.sign-up-message').text('There was an error creating the account. ', error).fadeIn('fast').delay(2000).fadeOut('slow')
 }
 
 const signInSuccess = (data) => {
+  // on user sign in, capture user info and assign it to store
   store.user = data.user
+  // displays elements based on login status
   $('.menu-button').show()
   $('.display').show()
   $('.form-clear').trigger('reset')
@@ -39,14 +46,16 @@ const passwordChangeFailure = function (error) {
 
 const allVegetablesSuccess = function (data) {
   store.vegetable = data.vegetable
+  // assigns data from handlebars file
   const displayVegetablesHTML = displayVegetablesTemplate({ vegetables: data.vegetables })
+  // ensures container is displayed
   $('.display-list').show()
+  // empties container to avoid duplicated entries
   $('.display-list').empty()
   $('.display').show()
   $('.instructions').text('Click Add to put vegetables in your garden list.')
-  $('.show-buttons').show(300)
+  // put data from handlebars file into the container with class .display-list and put the data at the beginning of the list
   $('.display-list').prepend(displayVegetablesHTML)
-  $('api-buttons').show()
 }
 const allVegetablesFailure = function (error) {
   $('.instructions').text('Oops, something went wrong.', error).fadeIn('fast').delay(2000).fadeOut('slow')
@@ -82,7 +91,8 @@ const getGardenSuccess = function (data) {
   $('.your-list').prepend(displayGardenHTML)
   $('.your-list').show()
 }
-const getGardenFailure = function (data) {
+const getGardenFailure = function (error) {
+  $('.instructions').text('Oops, something went wrong.', error).fadeIn('fast').delay(2000).fadeOut('slow')
 }
 
 const allPestsSuccess = function (data) {
@@ -92,11 +102,11 @@ const allPestsSuccess = function (data) {
   $('.pest-list').show()
 }
 
-const allPestsFailure = function (data) {
+const allPestsFailure = function (error) {
+  $('.instructions').text('Oops, something went wrong.', error).fadeIn('fast').delay(2000).fadeOut('slow')
 }
 
 const logoutSuccess = function () {
-  $('.api-buttons').hide()
   $('.form-clear').trigger('reset')
   $('.display').hide()
   $('.display-list').hide()
@@ -104,8 +114,6 @@ const logoutSuccess = function () {
   $('.login-buttons').show(900)
   $('.login-screen').show()
   $('#log-out').modal('hide')
-  $('.show-buttons').hide()
-  $('#user-buttons').hide()
   $('.menu-button').hide()
 }
 
